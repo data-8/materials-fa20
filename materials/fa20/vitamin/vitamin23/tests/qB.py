b64d759113fb68184e28c693caafff020982c6b0
Emily Zou <emilykzou@berkeley.edu>
2020-10-19 13:39:51 -0400

    fa20/vitamin/vitamin23.push

---
+++ materials/fa20/vitamin/vitamin23/tests/qB.py
@@ -0,0 +1,39 @@
+test = {
+  'name': 'qB',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Your answer is not one of the available choices!!;
+          >>> answer_B in [1, 2]
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> feedback.check_ans(answer_B, 2, "Please check it on Gradescope!")
+          False
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> feedback.correct(answer_B, 1, "If you'd like to, check out some of the links above if you haven't already.")
+          <IPython.core.display.Markdown object>
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
