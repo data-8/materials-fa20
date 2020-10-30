6b966b5b17707c24b024a2ccbef8d5f28119b5cf
Olivia Lewke <olivialewke@Olivias-MacBook-Pro.local>
2020-10-30 10:21:47 -0700

    fa20/vitamin/vitamin28.push

---
+++ materials/fa20/vitamin/vitamin28/tests/qA.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'qA',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Your answer is not one of the available choices!!;
+          >>> answer_A in [1, 2]
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
