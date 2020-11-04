b2dd89403cd33721f45847a438cf8b7a9a8778a5
Olivia Lewke <olivialewke@Olivias-MacBook-Pro.local>
2020-11-04 10:48:14 -0800

    fa20/vitamin/vitamin30.push

---
+++ materials/fa20/vitamin/vitamin30/tests/qB.py
@@ -0,0 +1,31 @@
+test = {
+  'name': 'qB',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Your answer is not one of the available choices!!;
+          >>> answer_B in [1, 2]
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> feedback.check_ans(answer_B, 2, "Correlation measures the **strength** of a linear relationship (if there is one) between two variables, so this strength should not change despite the flipping of variables.")
+          False
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
